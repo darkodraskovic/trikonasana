48d27667d1de913c682b9722b68297ab29f9ed19
Darko Draskovic <darko.draskovic@gmail.com>
2022-06-02 20:10:58 +0200

    Add configure and compile options to Windows build
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ ROOT_DIR=~/Development/trikonasana
 EXE_NAME=main
 CONFIGURE=false
 COMPILE_CMD="make"
+WIN=false
 
 while getopts ":c" option; do
     case $option in
@@ -14,21 +15,31 @@ while getopts ":c" option; do
 done
 
 if [ "$OSTYPE" = "msys" ]; then
-    EXE_NAME=${EXE_NAME}.exe
+    WIN=true
+    
     COMPILE_CMD="MSBuild.exe -target:Build /property:Configuration=Release main.vcxproj"
-else
-    EXE_NAME=./${EXE_NAME}
+    ROOT_DIR="c:/Users/darko/Development/trikonasana/"
 fi
 
+# configure
 if [ $CONFIGURE == true ]; then
     cd $ROOT_DIR/build/
     cmake ..
+
+    if  [ $WIN == true ]; then
+        cd $ROOT_DIR/ninja/
+        cmake -G"Ninja" ..
+        mv compile_commands.json ..
+    fi
 fi
 
 
+# build
 cd $ROOT_DIR/build/
 echo "COMPILE_CMD: $COMPILE_CMD"
 $COMPILE_CMD
+
+# run
 cd $ROOT_DIR/bin/
 echo "EXE_NAME: $EXE_NAME"
-$EXE_NAME
+./${EXE_NAME}
